8188d775d2e4bcc3bc6080f3e8c8fbdfa4e3317d
rod <rod@pu-gh.com>
2013-04-20 22:47:07 +0100

    more auto to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
 
+VIM="$HOME/.vim"
+VIMRC="$HOME/.vimrc"
+
+function fatal() {
+  echo "Error: $1"
+  echo ""
+  echo "(try: rm -rf ~/.vim*)"
+  exit 1
+}
+
+if [ -d $VIM ];
+then
+  fatal "Directory '$VIM' already exists"
+fi
+
+if [ -L $VIM ];
+then
+  fatal "'$VIM' is currently symlinked"
+fi
+
+if [ -e $VIMRC ];
+then
+  fatal "File '$VIMRC' already exists"
+fi
+
 ln -s `pwd`/vimrc $HOME/.vimrc
 ln -s `pwd`/vim $HOME/.vim
+
+echo "Symlinks created, installing vundles..."
+
+vim +BundleInstall +qall -e -s
+
+echo "Complete!"
 
